da7a36f46593964f9203736b1b3a392872dd7b9f
nccinfra <me@myemail.com>
2025-10-23 02:40:43 +0000

    refactoring the app and depency injection

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from models.servicenow import ServiceNow
 from models.agent import NetworkAgent
 from models.voice_agent import VoiceAgent
 from models.llm_factory import AbstractLLMServiceFactory
-from resources import llm_factoy_object, servicenow_instance, network_agent, voice_agent
 
 app = Flask(__name__)
 app.register_blueprint(alert_bp)
@@ -23,15 +22,14 @@ app.register_blueprint(onprem_bp)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
     
-    
     # create one instance of the abstract factory llm service class
-    llm_factoy_object = llm_factoy_object
+    llm_factoy_object = AbstractLLMServiceFactory()
     # create one instance for servicenow in the app
-    servicenow_instance = servicenow_instance
+    servicenow_instance = ServiceNow()
     # creae one instance of the Network Agent in the app
-    network_agent = network_agent
+    network_agent = NetworkAgent(servicenow_instance, llm_factoy_object)
     # create one instance of the voice agent in the app
-    voice_agent = voice_agent
+    voice_agent = VoiceAgent(servicenow_instance, llm_factoy_object)
 
     # Start autonomous agent servicenow work in a background thread
     agent_thread = threading.Thread(target=network_agent.servicenow_instance.start_servicenow, daemon=True)
--- resources.py
+++ resources.py
@@ -1,13 +1,1 @@
-from models.voice_agent import VoiceAgent
-from models.llm_factory import AbstractLLMServiceFactory
-from models.servicenow import ServiceNow
-from models.agent import NetworkAgent
-
-# create one instance of the abstract factory llm service class
-llm_factoy_object = AbstractLLMServiceFactory()
-# create one instance for servicenow in the app
-servicenow_instance = ServiceNow()
-# creae one instance of the Network Agent in the app
-network_agent = NetworkAgent(servicenow_instance, llm_factoy_object)
-# create one instance of the voice agent in the app
-voice_agent = VoiceAgent(servicenow_instance, llm_factoy_object)
+from app import voice_agent
